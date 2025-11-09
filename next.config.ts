fe502511fab953d8841596cc19731146eaad6827
Estratégia Chinesa <estrategiachinesa@gmail.com>
2025-11-09 22:15:39 +0000

    File changes

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,8 @@ const nextConfig: NextConfig = {
   // IMPORTANT: The build will be configured for a root deployment.
   // If you are deploying to a subdirectory (e.g., your-gh-username.github.io/your-repo-name),
   // you will need to add `basePath: '/your-repo-name'` and `assetPrefix: '/your-repo-name/'`.
-  basePath: '/i.a.',
-  assetPrefix: '/i.a./',
+  basePath: '/i.a..',
+  assetPrefix: '/i.a../',
   output: 'export',
   typescript: {
     ignoreBuildErrors: true,

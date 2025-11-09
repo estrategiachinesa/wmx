963647dff60c1e1c0be912a2bdfad45be670aa5f
Estratégia Chinesa <estrategiachinesa@gmail.com>
2025-11-09 22:03:31 +0000

    faça todas as modificações necessarias

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,12 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
   // IMPORTANT: The build will be configured for a root deployment.
   // If you are deploying to a subdirectory (e.g., your-gh-username.github.io/your-repo-name),
   // you will need to add `basePath: '/your-repo-name'` and `assetPrefix: '/your-repo-name/'`.
   basePath: '/traderchines',
   assetPrefix: '/traderchines/',
+  output: 'export',
   typescript: {
     ignoreBuildErrors: true,
   },

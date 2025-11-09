fe5369e3020bfd3be4188392a3d3778560e6c977
Estratégia Chinesa <estrategiachinesa@gmail.com>
2025-11-09 19:02:48 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "gerador-de-sinais-158130-b1a02",
+  "appId": "1:1025879039495:web:3feb990c958fc35e3254a2",
+  "apiKey": "AIzaSyCkJEYwmev4XuuDzOlTLnxM6wEDpE5nx9g",
+  "authDomain": "gerador-de-sinais-158130-b1a02.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1025879039495"
+};

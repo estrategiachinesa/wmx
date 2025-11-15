0563e4c2e58d9d31d5268161ad0a48d7a1deceb3
Estratégia Chinesa <estrategiachinesa@gmail.com>
2025-11-15 17:04:53 +0000

    mude para modo preview

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,12 @@
-import type {NextConfig} from 'next';
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: '/ia',
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: {
-    unoptimized: true,
-  },
 };
 
-// Forçando a sincronização para resolver o erro de push (non-fast-forward)
 export default nextConfig;

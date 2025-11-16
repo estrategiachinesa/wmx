5c71d10f13299ada45cc94d318590a664fe80e39
Estratégia Chinesa <estrategiachinesa@gmail.com>
2025-11-16 00:32:02 +0000

    estamos no caminho certo, o deploy foi feito, agora falta sair o nosso p

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from 'next';
 
+const repo = 'ia';
+const assetPrefix = `/${repo}/`;
+const basePath = `/${repo}`;
+
 const nextConfig: NextConfig = {
   output: 'export',
+  assetPrefix: assetPrefix,
+  basePath: basePath,
   typescript: {
     ignoreBuildErrors: true,
   },

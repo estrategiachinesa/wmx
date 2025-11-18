e0bb99408579488325717cef6247e558eff9aea6
Estratégia Chinesa <estrategiachinesa@gmail.com>
2025-11-18 19:50:45 +0000

    estamos no caminho certo, o deploy foi feito, mas a pagina saiu assim

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 
 import type { NextConfig } from 'next';
 
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: isProd ? '/wm' : undefined,
+  assetPrefix: isProd ? '/wm/' : undefined,
   typescript: {
     ignoreBuildErrors: true,
   },

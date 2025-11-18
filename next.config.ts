e2cca5718bc565a79ee47a63f9e8dd69ebb7458f
Estratégia Chinesa <estrategiachinesa@gmail.com>
2025-11-18 04:37:09 +0000

    deploy foi feito, mas ficou assim
    esse é o link do repositorio correto

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/will',
+  assetPrefix: '/will',
   typescript: {
     ignoreBuildErrors: true,
   },

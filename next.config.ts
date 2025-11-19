7c356b9773eae4690c84f82199c65653cb65231a
Estratégia Chinesa <estrategiachinesa@gmail.com>
2025-11-19 02:17:14 +0000

    a pagina esta assim

--- next.config.ts
+++ next.config.ts
@@ -5,8 +5,6 @@ const isProd = process.env.NODE_ENV === 'production'
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: isProd ? '/wm' : undefined,
-  assetPrefix: isProd ? '/wm/' : undefined,
   typescript: {
     ignoreBuildErrors: true,
   },
